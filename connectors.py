1cad19da5a2f426901f377f48cd7f018d0bf706a
Meska <meskatech@gmail.com>
2015-06-29 00:35:19 +0200

    keep parser on main app

--- connectors.py
+++ connectors.py
@@ -2,8 +2,8 @@ from telegrambot.signals import message_received
 from django.dispatch import receiver
 
 
-@receiver(message_received)
-def receive_message(sender, **kwargs):
-    from parser import Parser
-    p = Parser(sender)
-    p.parse(kwargs['message'])
+#@receiver(message_received)
+#def receive_message(sender, **kwargs):
+    #from parser import Parser
+    #p = Parser(sender)
+    #p.parse(kwargs['message'])
