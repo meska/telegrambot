02d26e11fee1f167c4b9e0c8179cb4d3bad033de
Meska <meskatech@gmail.com>
2015-06-30 08:52:33 +0200

    bugfix

--- apps.py
+++ apps.py
@@ -12,13 +12,13 @@ class TelegramBotConfig(AppConfig):
             from django.conf import settings
             from telegrambot.wrapper import Bot
             b = Bot(settings.TELEGRAM_BOT_TOKEN)
-            if settings.TELEGRAM_USE_WEBHOOK:
-                b.setWebhook()
-            else:
+            if not settings.TELEGRAM_USE_WEBHOOK:
                 b.post('setWebhook',{'url':''})
                 print("Telegram WebHook Disabled")
                 Timer(10,b.getUpdates).start()
                 
-               
+        if settings.TELEGRAM_USE_WEBHOOK:
+            b.setWebhook()               
+
         import telegrambot.signals
         import telegrambot.connectors
